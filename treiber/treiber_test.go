bd2a7a5ca64a2a6629b80da6126e64064b5972be
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-05-23 14:34:41 +0900

    added test case

--- treiber/treiber_test.go
+++ treiber/treiber_test.go
@@ -1,1 +1,49 @@
 package treiber
+
+import (
+	"testing"
+)
+
+func TestStackOperation(t *testing.T) {
+	stack := NewStack()
+
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+
+	value, err := stack.Pop()
+	if err != nil {
+		t.Errorf("Pop() error: %v", value)
+	}
+
+	if value != 3 {
+		t.Errorf("Pop() value expected: %v, got: %v", 3, value)
+	}
+
+	stack.Pop()
+	stack.Pop()
+
+	if !stack.IsEmpty() {
+		t.Errorf("IsEmpty() expected: %v, got: %v", true, stack.IsEmpty())
+	}
+
+	stack.Push(4)
+
+	value, err = stack.Pop()
+	if err != nil {
+		t.Errorf("Pop() error: %v", value)
+	}
+
+	if value != 4 {
+		t.Errorf("Pop() value expected: %v, got: %v", 4, value)
+	}
+
+	value, err = stack.Pop()
+	if err == nil {
+		t.Error("Pop() error is nil")
+	}
+
+	if value != nil {
+		t.Error("Pop() value is not nil")
+	}
+}
