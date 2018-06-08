d5ad40d050c67627619f234e39ce244d41bc585b
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-06-08 15:29:13 +0900

    added benchmark test

---
+++ gompool_benchmark_test.go
@@ -0,0 +1,45 @@
+package gompool
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+)
+
+func BenchmarkGompool(b *testing.B) {
+	pools := NewGompool(100, func() interface{} {
+		return &bytes.Buffer{}
+	})
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		pool1, _ := pools.Get()
+		_ = pool1.Value.(*bytes.Buffer)
+
+		pool2, _ := pools.Get()
+		_ = pool2.Value.(*bytes.Buffer)
+
+		pools.Put(pool1)
+		pools.Put(pool2)
+	}
+}
+
+func BenchmarkDefaultPool(b *testing.B) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return &bytes.Buffer{}
+		},
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf1 := pool.Get().(*bytes.Buffer)
+
+		buf2 := pool.Get().(*bytes.Buffer)
+
+		pool.Put(buf1)
+		pool.Put(buf2)
+	}
+}
